ec1ffab1873e4cea7c705bbc3fc2a9d2f58b7861
ChengCK18 <chinkheng_98@hotmail.com>
2024-04-17 22:08:21 +0800

    Simple usage of useContext

--- context/src/App.jsx
+++ context/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import CountPanel from "./components/CountPanel";
 import CountProvider from "./provider/CountProvider";
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <div>
             <CountProvider>
